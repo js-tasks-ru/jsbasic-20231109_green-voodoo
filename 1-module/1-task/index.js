97f8beb2c9e72f14c6a017734e0d768ecd4d7151
ProskurinR <green_voodoo@mail.ru>
2023-11-13 23:04:23 +0300

    2 task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,15 @@
 function factorial(n) {
-  // ваш код...
+  if (n >= 0 && n % 1 == 0) {
+    if (n == 0 || n == 1) {
+      return 1;
+    } else {
+      let f = 1;
+      for (i = 2; i <= n; i++) {
+        f = f * i;
+      }
+      return f;
+    }
+  } else {
+    console.log("Не натуральное число!");
+  }
 }
