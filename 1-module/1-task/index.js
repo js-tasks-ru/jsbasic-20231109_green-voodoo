8be99cc361518a9a3dedd866edf14fc1fe1b39d0
ProskurinR <green_voodoo@mail.ru>
2023-11-14 19:43:15 +0300

    code correction

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,15 +1,16 @@
 function factorial(n) {
-  if (n >= 0 && n % 1 == 0) {
-    if (n == 0 || n == 1) {
-      return 1;
-    } else {
-      let f = 1;
-      for (i = 2; i <= n; i++) {
-        f = f * i;
-      }
-      return f;
-    }
-  } else {
+  if (n < 0 || n % 1 !== 0) {
     console.log("Не натуральное число!");
+    return;
   }
+
+  if (n == 0 || n == 1) {
+    return 1;
+  }
+
+  let f = 1;
+  for (i = 2; i <= n; i++) {
+    f = f * i;
+  }
+  return f;
 }
