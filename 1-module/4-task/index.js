413cd4d2cab74a994af0777b5f60921e730e75b8
ProskurinR <green_voodoo@mail.ru>
2023-11-16 15:20:14 +0300

    task_4_5_6

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,16 @@
 function checkSpam(str) {
   // ваш код...
+  if (!str) {
+    return false;
+  }
+
+  str = str.toLowerCase();
+  if (
+    str.includes("1xBet".toLowerCase()) ||
+    str.includes("XXX".toLowerCase())
+  ) {
+    return true;
+  }
+
+  return false;
 }
