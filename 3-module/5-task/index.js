b4fc375a0ab4da6aae7974015661b60758a2af7e
ProskurinR <green_voodoo@mail.ru>
2023-11-21 17:30:24 +0300

    3_module 4_5_task fix

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -2,10 +2,8 @@ function getMinMax(str) {
   // ваш код...
   let arrNumber = str.split(" ").filter((item) => Number.isFinite(+item));
 
-  let result = {};
-
-  result.min = Math.min(...arrNumber);
-  result.max = Math.max(...arrNumber);
-
-  return result;
+  return {
+    min: Math.min(...arrNumber),
+    max: Math.max(...arrNumber),
+  };
 }
