f664488f6a5d90aee91f656e85359df1a02acf3b
ProskurinR <green_voodoo@mail.ru>
2023-11-24 23:38:37 +0300

    4_module 1_2_3_task

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,46 @@
 function highlight(table) {
   // ваш код...
+  function getCellIndex(cellValue) {
+    for (let i = 0; i < table.rows[0].cells.length; i++) {
+      if (table.rows[0].cells[i].innerHTML == cellValue) {
+        return i;
+      }
+    }
+  }
+
+  let classRow;
+
+  let cellStatus;
+  let cellGender;
+  let cellAge;
+
+  for (i = 1; i < table.rows.length; i++) {
+    classRow = table.rows[i].classList;
+
+    cellStatus = table.rows[i].cells[getCellIndex("Status")];
+    switch (cellStatus.dataset.available) {
+      case "true":
+        classRow += " available";
+        break;
+      case "false":
+        classRow += " unavailable";
+        break;
+      default:
+        table.rows[i].setAttribute("hidden", true);
+    }
+
+    cellGender = table.rows[i].cells[getCellIndex("Gender")];
+    if (cellGender.innerHTML == "m") {
+      classRow += " male";
+    } else if (cellGender.innerHTML == "f") {
+      classRow += " female";
+    }
+
+    table.rows[i].className = classRow;
+
+    cellAge = table.rows[i].cells[getCellIndex("Age")];
+    if (+cellAge.innerHTML < 18) {
+      table.rows[i].style = "text-decoration: line-through";
+    }
+  }
 }
