05b80b1b6463069d9e4c127c52e5b3ef64a0cd84
ProskurinR <green_voodoo@mail.ru>
2023-11-29 20:46:41 +0300

    5_module 1_2_3_task

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,15 @@
 function toggleText() {
   // ваш код...
+  let button = document.querySelector(".toggle-text-button");
+  let text = document.querySelector("#text");
+
+  let onClick = (e) => {
+    text.hasAttribute("hidden")
+      ? text.removeAttribute("hidden")
+      : text.setAttribute("hidden", true);
+
+    e.stopPropagation();
+  };
+
+  button.addEventListener("click", onClick, false);
 }
