88f7c57852ea58bb9e5566ece522a7a31343a507
ProskurinR <green_voodoo@mail.ru>
2023-12-06 10:01:36 +0300

    6_module__1_2_3_task

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,49 @@
  *
  */
 export default class UserTable {
+  elem = null;
+  #rows = [];
+
   constructor(rows) {
+    this.#rows = rows || this.#rows;
+    this.elem = this.#render();
+  }
+
+  #render() {
+    this.elem = document.createElement("table");
+    let template = `<thead>
+      <tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+        <th></th>
+      </tr>
+    </thead>`;
+
+    this.elem.insertAdjacentHTML("afterbegin", template);
+
+    template = ``;
+
+    for (let i = 0; i < this.#rows.length; i++) {
+      template += `<tr>`;
+
+      for (let key in this.#rows[i]) {
+        template += `<td>${this.#rows[i][key]}</td>`;
+      }
+
+      template += `<td><button>X</button></td>
+        </tr>`;
+    }
+
+    this.elem.insertAdjacentHTML("beforeend", template);
+
+    this.elem.addEventListener("click", (e) => {
+      if (e.target.closest("button")) {
+        e.target.parentNode.parentNode.remove();
+      }
+    });
+
+    return this.elem;
   }
 }
