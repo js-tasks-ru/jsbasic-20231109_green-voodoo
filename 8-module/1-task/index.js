e291c9ad024e51f86fcfe869aed5648f4f54b87e
ProskurinR <green_voodoo@mail.ru>
2023-12-15 22:10:09 +0300

    8_module

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -1,9 +1,8 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
 
 export default class CartIcon {
   constructor() {
     this.render();
-
     this.addEventListeners();
   }
 
@@ -13,32 +12,78 @@ export default class CartIcon {
 
   update(cart) {
     if (!cart.isEmpty()) {
-      this.elem.classList.add('cart-icon_visible');
+      this.elem.classList.add("cart-icon_visible");
 
       this.elem.innerHTML = `
         <div class="cart-icon__inner">
           <span class="cart-icon__count">${cart.getTotalCount()}</span>
-          <span class="cart-icon__price">€${cart.getTotalPrice().toFixed(2)}</span>
+          <span class="cart-icon__price">€${cart
+            .getTotalPrice()
+            .toFixed(2)}</span>
         </div>`;
 
       this.updatePosition();
 
-      this.elem.classList.add('shake');
-      this.elem.addEventListener('transitionend', () => {
-        this.elem.classList.remove('shake');
-      }, {once: true});
-
+      this.elem.classList.add("shake");
+      this.elem.addEventListener(
+        "transitionend",
+        () => {
+          this.elem.classList.remove("shake");
+        },
+        { once: true }
+      );
     } else {
-      this.elem.classList.remove('cart-icon_visible');
+      this.elem.classList.remove("cart-icon_visible");
     }
   }
 
   addEventListeners() {
-    document.addEventListener('scroll', () => this.updatePosition());
-    window.addEventListener('resize', () => this.updatePosition());
+    document.addEventListener("scroll", () => this.updatePosition());
+    window.addEventListener("resize", () => this.updatePosition());
   }
 
   updatePosition() {
     // ваш код ...
+    if (this.elem.offsetHeight === 0) return;
+
+    if (document.documentElement.clientWidth <= 767) {
+      this.elem.style.position = "";
+      this.elem.style.top = "";
+      this.elem.style.right = "";
+      this.elem.style.left = "";
+      return;
+    }
+
+    let getDefaultPosition = () => {
+      let defaultPosition =
+        this.elem.getBoundingClientRect().top + window.pageYOffset;
+
+      return function () {
+        return window.pageYOffset > defaultPosition;
+      };
+    };
+
+    let getDefault = getDefaultPosition;
+    let deltaDefault = getDefault();
+
+    if (deltaDefault) {
+      let elemContainer = document.querySelector(".container");
+
+      let leftPosition =
+        Math.min(
+          elemContainer.getBoundingClientRect().right + 20,
+          document.documentElement.clientWidth - this.elem.offsetWidth - 10
+        ) + "px";
+
+      this.elem.style.position = "fixed";
+      this.elem.style.top = "50px";
+      this.elem.style.right = "10px";
+      this.elem.style.left = leftPosition;
+    } else {
+      this.elem.style.position = "";
+      this.elem.style.top = "";
+      this.elem.style.right = "";
+      this.elem.style.left = "";
+    }
   }
 }
--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -1,9 +1,69 @@
-import createElement from '../../assets/lib/create-element.js';
-import ProductCard from '../../6-module/2-task/index.js';
+import createElement from "../../assets/lib/create-element.js";
+import ProductCard from "../../6-module/2-task/index.js";
 
 export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+    this.elem = this.#render();
+  }
+
+  updateFilter(filters) {
+    this.elem.firstElementChild.innerHTML = "";
+
+    for (let key in filters) {
+      this.filters[key] = filters[key];
+    }
+
+    this.elem.firstElementChild.innerHTML = this.#addInnerHTML();
+  }
+
+  #isShow(product, filters) {
+    if (product.nuts && filters.noNuts && filters.noNuts === true) return false;
+    if (
+      (!product.vegeterian || product.vegeterian === false) &&
+      filters.vegeterianOnly &&
+      filters.vegeterianOnly === true
+    )
+      return false;
+    if (
+      product.spiciness &&
+      filters.maxSpiciness &&
+      filters.maxSpiciness < product.spiciness
+    )
+      return false;
+    if (
+      product.category &&
+      filters.category &&
+      filters.category !== product.category
+    )
+      return false;
+
+    return true;
+  }
+
+  #addInnerHTML() {
+    let template = "";
+    let productCard;
+    for (let i = 0; i < this.products.length; i++) {
+      if (this.#isShow(this.products[i], this.filters)) {
+        productCard = new ProductCard(this.products[i]);
+        template +=
+          `<div class="card">` + productCard.elem.innerHTML + `</div>`;
+      }
+    }
+    return template;
+  }
+
+  #render() {
+    let templateAll = `<div class="products-grid">
+    <div class="products-grid__inner">
+    ${this.#addInnerHTML()}
+    </div>
+    </div>`;
+
+    this.elem = createElement(templateAll);
+
+    return this.elem;
   }
 }
